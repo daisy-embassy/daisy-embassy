5275103075c545cae1172c3c725ebe6988e38fca
Dickless <011001nobuo@gmail.com>
2025-06-15 00:53:01 +0900

    Update edition to 2024 in Cargo.toml and fix errors and warnings by the edition migration.

--- src/audio.rs
+++ src/audio.rs
@@ -17,9 +17,9 @@ pub const DMA_BUFFER_LENGTH: usize = HALF_DMA_BUFFER_LENGTH * 2; //  2 half-bloc
 // - static data --------------------------------------------------------------
 
 //DMA buffer must be in special region. Refer https://embassy.dev/book/#_stm32_bdma_only_working_out_of_some_ram_regions
-#[link_section = ".sram1_bss"]
+#[unsafe(link_section = ".sram1_bss")]
 static TX_BUFFER: GroundedArrayCell<u32, DMA_BUFFER_LENGTH> = GroundedArrayCell::uninit();
-#[link_section = ".sram1_bss"]
+#[unsafe(link_section = ".sram1_bss")]
 static RX_BUFFER: GroundedArrayCell<u32, DMA_BUFFER_LENGTH> = GroundedArrayCell::uninit();
 
 // - types --------------------------------------------------------------------
