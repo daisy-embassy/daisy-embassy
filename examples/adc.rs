9790249d41f1cea0e68f823ca3a6b285c4894f08
Dr-McFish <61105022+Dr-McFish@users.noreply.github.com>
2025-12-07 14:24:20 +0100

    Change the memory location of buffer as per the recomendation of @orukusaki
    This fixes the issue and it works perfectly now

--- examples/adc.rs
+++ examples/adc.rs
@@ -1,5 +1,5 @@
 // controls the speed of the blinking onboard LED using a potentiometer
-// Connect a potentiometer to pin ADC1/D16
+// Connect the middle pin/wiper of the potentiometer to pin ADC1/D16, connect bottom pin to ground, and top pin to 3V3 Analogue(pin 21).
 
 #![no_std]
 #![no_main]
@@ -12,11 +12,15 @@ use embassy_stm32::adc::{Adc, AdcChannel as _, SampleTime};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::signal::Signal;
 use embassy_time::Timer;
+use grounded::uninit::GroundedArrayCell;
 use {defmt_rtt as _, panic_probe as _};
 
 use {defmt_rtt as _, panic_probe as _};
 
 static PERIOD_CONTROL: Signal<CriticalSectionRawMutex, u16> = Signal::new();
+
+#[unsafe(link_section = ".sram1_bss")]
+static ADC_BUFFER: GroundedArrayCell<u16, 2> = GroundedArrayCell::uninit();
 
 #[embassy_executor::task]
 async fn blink(mut led: UserLed<'static>) {
@@ -46,13 +50,19 @@ async fn main(spawner: Spawner) {
     let daisy_p = new_daisy_board!(p);
     spawner.spawn(blink(daisy_p.user_led)).unwrap();
 
-    let mut read_buffer: [u16; 2] = [0; 2];
+    // let mut read_buffer: [u16; 2] = [0; 2];
+    let adc_buffer: &mut [u16] = unsafe {
+        ADC_BUFFER.initialize_all_copied(0);
+        let (ptr, len) = ADC_BUFFER.get_ptr_len();
+        core::slice::from_raw_parts_mut(ptr, len)
+    };
+
     let mut adc = Adc::new(p.ADC1);
     let mut vrefint_channel = adc.enable_vrefint().degrade_adc();
     let mut pc0 = daisy_p.pins.d16.degrade_adc();
 
     loop {
-        Timer::after_millis(1_000).await;
+        Timer::after_millis(10).await;
         adc.read(
             &mut p.DMA2_CH1,
             [
@@ -60,13 +70,13 @@ async fn main(spawner: Spawner) {
                 (&mut pc0, SampleTime::CYCLES387_5),
             ]
             .into_iter(),
-            &mut read_buffer,
+            adc_buffer,
         )
         .await;
 
-        let vrefint = read_buffer[0];
+        let vrefint = adc_buffer[0];
         info!("vrefint: {}", vrefint);
-        let measured = read_buffer[1];
+        let measured = adc_buffer[1];
         info!("measured: {}", measured);
         PERIOD_CONTROL.signal(measured);
     }
