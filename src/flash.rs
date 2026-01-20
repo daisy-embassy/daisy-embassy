4bb2a424f6bbb754dae34ccb96d90f6741517d57
Peter Smith <psmith@inviqa.com>
2026-01-20 03:42:08 +0000

    Fix qspi dummy cycles (#62)
    * don't use QPI mode
    
    * reset qspi flash chip during init
    
    * set dummy cycles to 8
    
    * fix read_uuid settings, update comments
    
    * fix read_uuid dwidth

--- src/flash.rs
+++ src/flash.rs
@@ -1,3 +1,9 @@
+//! Driver for the IS25LP064 Flash chip connected via QSPI
+//! Notes:
+//! 1. The Qspi driver in embassy_stm32 can currently only be used in Blocking mode, because Async would require MDMA, which is unsupported.
+//! 2. The Daisy bootloader (as of v6.3) Does not use QPI mode, and configuring the flash chip that way would cause problems on reset. So for compatibility's sake, we do not use it here either.
+#![allow(unused)]
+
 use crate::hal;
 use crate::pins::FlashPins;
 use embassy_stm32::qspi::enums::{AddressSize, ChipSelectHighTime, FIFOThresholdLevel, MemorySize};
@@ -11,18 +17,38 @@ use hal::{
 };
 
 // Commands from IS25LP064 datasheet.
-const WRITE_STATUS_REGISTRY_CMD: u8 = 0x01; // WRSR
-const WRITE_CMD: u8 = 0x02; // PP
-const READ_STATUS_REGISTRY_CMD: u8 = 0x05; // RDSR
+const WRITE_CMD: u8 = 0x32; // PPQ
 const WRITE_ENABLE_CMD: u8 = 0x06; // WREN
-const ENTER_QPI_MODE_CMD: u8 = 0x35; // QPIEN
-const SET_READ_PARAMETERS_CMD: u8 = 0xC0; // SRP
 const SECTOR_ERASE_CMD: u8 = 0xD7; // SER
 const FAST_READ_QUAD_IO_CMD: u8 = 0xEB; // FRQIO
+const RESET_ENABLE_CMD: u8 = 0x66;
+const RESET_MEMORY_CMD: u8 = 0x99;
+
+const WRITE_STATUS_REGISTER_CMD: u8 = 0x01; // WRSR
+const READ_STATUS_REGISTER_CMD: u8 = 0x05; // RDSR
+const STATUS_BIT_WIP: u8 = 1 << 0;
+const STATUS_BIT_WEL: u8 = 1 << 1;
+const STATUS_BIT_BP0: u8 = 1 << 2;
+const STATUS_BIT_BP1: u8 = 1 << 3;
+const STATUS_BIT_BP2: u8 = 1 << 4;
+const STATUS_BIT_BP3: u8 = 1 << 5;
+const STATUS_BIT_QE: u8 = 1 << 6;
+const STATUS_BIT_SRWD: u8 = 1 << 7;
+
+const SET_READ_PARAMETERS_CMD: u8 = 0xC0; // SRP
+const READ_PARAMS_BIT_BL0: u8 = 1 << 0;
+const READ_PARAMS_BIT_BL1: u8 = 1 << 1;
+const READ_PARAMS_BIT_WE: u8 = 1 << 2;
+const READ_PARAMS_BIT_DC0: u8 = 1 << 3;
+const READ_PARAMS_BIT_DC1: u8 = 1 << 4;
+const READ_PARAMS_BIT_ODS0: u8 = 1 << 5;
+const READ_PARAMS_BIT_ODS1: u8 = 1 << 6;
+const READ_PARAMS_BIT_ODS2: u8 = 1 << 7;
 
 // Memory array specifications as defined in the datasheet.
 const SECTOR_SIZE: u32 = 4096;
 const PAGE_SIZE: u32 = 256;
+const MAX_ADDRESS: u32 = 0x7FFFFF;
 
 pub struct FlashBuilder {
     pub pins: FlashPins,
@@ -43,13 +69,12 @@ impl FlashBuilder {
             qspi, pins.IO0, pins.IO1, pins.IO2, pins.IO3, pins.SCK, pins.CS, config,
         );
         let mut result = Flash { qspi };
-        result.enable_qpi_mode();
+        result.reset_memory();
         result.reset_status_register();
         result.reset_read_register();
         result
     }
 }
-const MAX_ADDRESS: u32 = 0x7FFFFF;
 
 pub struct Flash<'a> {
     qspi: Qspi<'a, QUADSPI, Blocking>,
@@ -57,15 +82,15 @@ pub struct Flash<'a> {
 
 impl Flash<'_> {
     pub fn read(&mut self, address: u32, buffer: &mut [u8]) {
-        assert!(address <= MAX_ADDRESS);
+        assert!(address + buffer.len() as u32 <= MAX_ADDRESS);
 
         let transaction = TransferConfig {
-            iwidth: QspiWidth::QUAD,
+            iwidth: QspiWidth::SING,
             awidth: QspiWidth::QUAD,
             dwidth: QspiWidth::QUAD,
             instruction: FAST_READ_QUAD_IO_CMD,
             address: Some(address),
-            dummy: DummyCycles::_6,
+            dummy: DummyCycles::_8,
         };
         self.qspi.blocking_read(buffer, transaction);
     }
@@ -73,12 +98,12 @@ impl Flash<'_> {
     pub fn read_uuid(&mut self) -> [u8; 16] {
         let mut buffer = [0; 16];
         let transaction: TransferConfig = TransferConfig {
-            iwidth: QspiWidth::QUAD,
-            awidth: QspiWidth::QUAD,
-            dwidth: QspiWidth::QUAD,
+            iwidth: QspiWidth::SING,
+            awidth: QspiWidth::SING,
+            dwidth: QspiWidth::SING,
             instruction: 0x4B,
             address: Some(0x00),
-            dummy: DummyCycles::_6,
+            dummy: DummyCycles::_8,
         };
         self.qspi.blocking_read(&mut buffer, transaction);
         buffer
@@ -92,7 +117,7 @@ impl Flash<'_> {
         let mut length = data.len() as u32;
         let mut start_cursor = 0;
 
-        //WRITE_CMD(or PP) allows to write up to 256 bytes, which is as much as PAGE_SIZE.
+        //WRITE_CMD(or PPQ) allows to write up to 256 bytes, which is as much as PAGE_SIZE.
         //Let's divide the data into chunks of page size to write to flash
         loop {
             // Calculate number of bytes between address and end of the page.
@@ -100,8 +125,8 @@ impl Flash<'_> {
             let size = page_remainder.min(length) as usize;
             self.enable_write();
             let transaction = TransferConfig {
-                iwidth: QspiWidth::QUAD,
-                awidth: QspiWidth::QUAD,
+                iwidth: QspiWidth::SING,
+                awidth: QspiWidth::SING,
                 dwidth: QspiWidth::QUAD,
                 instruction: WRITE_CMD,
                 address: Some(address),
@@ -133,8 +158,8 @@ impl Flash<'_> {
             // Erase the sector.
             self.enable_write();
             let transaction = TransferConfig {
-                iwidth: QspiWidth::QUAD,
-                awidth: QspiWidth::QUAD,
+                iwidth: QspiWidth::SING,
+                awidth: QspiWidth::SING,
                 dwidth: QspiWidth::NONE,
                 instruction: SECTOR_ERASE_CMD,
                 address: Some(address),
@@ -161,7 +186,7 @@ impl Flash<'_> {
 
     fn enable_write(&mut self) {
         let transaction = TransferConfig {
-            iwidth: QspiWidth::QUAD,
+            iwidth: QspiWidth::SING,
             awidth: QspiWidth::NONE,
             dwidth: QspiWidth::NONE,
             instruction: WRITE_ENABLE_CMD,
@@ -173,66 +198,81 @@ impl Flash<'_> {
 
     fn wait_for_write(&mut self) {
         loop {
-            let mut status: [u8; 1] = [0xFF; 1];
-            let transaction = TransferConfig {
-                iwidth: QspiWidth::QUAD,
-                awidth: QspiWidth::NONE,
-                dwidth: QspiWidth::QUAD,
-                instruction: READ_STATUS_REGISTRY_CMD,
-                address: None,
-                dummy: DummyCycles::_0,
-            };
-            self.qspi.blocking_read(&mut status, transaction);
-
-            if status[0] & 0x01 == 0 {
+            if self.read_status() & STATUS_BIT_WIP == 0 {
                 break;
             }
         }
+    }
+
+    fn read_status(&mut self) -> u8 {
+        let mut status: [u8; 1] = [0xFF; 1];
+        let transaction = TransferConfig {
+            iwidth: QspiWidth::SING,
+            awidth: QspiWidth::NONE,
+            dwidth: QspiWidth::SING,
+            instruction: READ_STATUS_REGISTER_CMD,
+            address: None,
+            dummy: DummyCycles::_0,
+        };
+        self.qspi.blocking_read(&mut status, transaction);
+        status[0]
+    }
+
+    fn reset_memory(&mut self) {
+        let transaction = TransferConfig {
+            iwidth: QspiWidth::SING,
+            awidth: QspiWidth::NONE,
+            dwidth: QspiWidth::NONE,
+            instruction: RESET_ENABLE_CMD,
+            address: None,
+            dummy: DummyCycles::_0,
+        };
+        self.qspi.blocking_command(transaction);
+
+        let transaction = TransferConfig {
+            iwidth: QspiWidth::SING,
+            awidth: QspiWidth::NONE,
+            dwidth: QspiWidth::NONE,
+            instruction: RESET_MEMORY_CMD,
+            address: None,
+            dummy: DummyCycles::_0,
+        };
+        self.qspi.blocking_command(transaction);
     }
 
     /// Reset status registers into driver's defaults. This makes sure that the
     /// peripheral is configured as expected.
     fn reset_status_register(&mut self) {
         self.enable_write();
+        let value = STATUS_BIT_QE;
         let transaction = TransferConfig {
-            iwidth: QspiWidth::QUAD,
-            awidth: QspiWidth::QUAD,
-            dwidth: QspiWidth::NONE,
-            instruction: WRITE_STATUS_REGISTRY_CMD,
-            address: Some(0b0000_0010),
+            iwidth: QspiWidth::SING,
+            awidth: QspiWidth::NONE,
+            dwidth: QspiWidth::SING,
+            instruction: WRITE_STATUS_REGISTER_CMD,
+            address: None,
             dummy: DummyCycles::_0,
         };
-        self.qspi.blocking_command(transaction);
+        self.qspi.blocking_write(&[value], transaction);
         self.wait_for_write();
     }
 
     /// Reset read registers into driver's defaults. This makes sure that the
     /// peripheral is configured as expected.
     fn reset_read_register(&mut self) {
-        self.enable_write();
-        let transaction = TransferConfig {
-            iwidth: QspiWidth::QUAD,
-            awidth: QspiWidth::QUAD,
-            dwidth: QspiWidth::NONE,
-            instruction: SET_READ_PARAMETERS_CMD,
-            address: Some(0b1111_1000),
-            dummy: DummyCycles::_0,
-        };
-        self.qspi.blocking_command(transaction);
-        self.wait_for_write();
-    }
-
-    fn enable_qpi_mode(&mut self) {
-        self.enable_write();
+        let value = READ_PARAMS_BIT_ODS2
+            | READ_PARAMS_BIT_ODS1
+            | READ_PARAMS_BIT_ODS0
+            | READ_PARAMS_BIT_DC1;
         let transaction = TransferConfig {
             iwidth: QspiWidth::SING,
             awidth: QspiWidth::NONE,
-            dwidth: QspiWidth::NONE,
-            instruction: ENTER_QPI_MODE_CMD,
+            dwidth: QspiWidth::SING,
+            instruction: SET_READ_PARAMETERS_CMD,
             address: None,
             dummy: DummyCycles::_0,
         };
-        self.qspi.blocking_command(transaction);
+        self.qspi.blocking_write(&[value], transaction);
         self.wait_for_write();
     }
 }
